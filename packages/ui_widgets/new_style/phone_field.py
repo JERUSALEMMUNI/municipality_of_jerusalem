dca058120e0b440c39e9f90abe06c637ed99aa89
wisamokkeh <wisam.okkeh@ness-tech.co.il>
2023-03-13 16:16:53 +0200

    edit phone field files
    added the ability to enter phone number into two parts and check each one separately

--- packages/ui_widgets/new_style/phone_field.py
+++ packages/ui_widgets/new_style/phone_field.py
@@ -1,6 +1,7 @@
 from selenium.webdriver.common.by import By
-from ui_widgets.base_widget import BaseWidget
+
 from infra import logger
+from ui_widgets.base_widget import BaseWidget
 from ui_widgets.new_style.dropdown_field import Dropdown
 from ui_widgets.new_style.text_field import TextField
 from ui_widgets.new_style.widget_locators.phone_field_locator import PhoneFieldLocators
@@ -9,10 +10,10 @@ log = logger.get_logger(__name__)
 
 
 class PhoneField(BaseWidget):
-    def __init__(self, label,index):
-        super().__init__(label,index)
-        self.dropdown_widget = Dropdown(label,index)
-        self.text_widget = TextField(label,index)
+    def __init__(self, label, index):
+        super().__init__(label, index)
+        self.dropdown_widget = Dropdown(label, index)
+        self.text_widget = TextField(label, index)
 
     @property
     def locator(self):
@@ -28,7 +29,16 @@ class PhoneField(BaseWidget):
         text_element = self.web_element.find_element(*PhoneFieldLocators.text_element)
         self.text_widget.set_web_element(text_element)
 
-    def set_text(self, text):
+    def set_prefix_number(self, start_phone):
+        self.initial_widgets()
+        self.dropdown_widget.click_button()
+        self.dropdown_widget.select_element(start_phone)
+
+    def set_phone_number(self, rest_phone):
+        self.initial_widgets()
+        self.text_widget.set_text(rest_phone)
+
+    def set_full_phone(self, text):
         new_text = text.split("-")
         start_phone = new_text[0]
         rest_phone = new_text[1]
