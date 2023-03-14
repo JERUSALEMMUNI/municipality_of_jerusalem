c5e70b2cd4a12477b902f5ae6aa9d4c97817ca36
DarweeshQaimari <125342803+DarweeshQaimari@users.noreply.github.com>
2023-03-14 21:47:10 +0200

    shortening xpath

--- packages/ui_widgets/new_style/widget_locators/dropdown_locators.py
+++ packages/ui_widgets/new_style/widget_locators/dropdown_locators.py
@@ -2,7 +2,7 @@ from selenium.webdriver.common.by import By
 
 
 class DropdownLocators:
-    dropDown_open = (By.XPATH, "./div/div/input")
+    dropDown_open = (By.XPATH, ".//input")
     item_search_scroll_element = (By.XPATH, "//div/div/div/ul")
     read_text = (By.XPATH, "./div/span")
     check_if_open = (By.XPATH, "./div")
