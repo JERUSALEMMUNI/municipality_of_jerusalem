3c5320994366d7ab9e186d7b7fe079b29367afd0
wisamokkeh <wisam.okkeh@ness-tech.co.il>
2023-03-15 16:26:47 +0200

    Merge branch 'master' of C:\Users\LENOVO\Documents\GitHub\team\municipality_of_jerusalem with conflicts.

--- packages/ui_widgets/new_style/widget_locators/dropdown_locators.py
+++ packages/ui_widgets/new_style/widget_locators/dropdown_locators.py
@@ -2,6 +2,7 @@ from selenium.webdriver.common.by import By
 
 
 class DropdownLocators:
+    # dropDown_open = (By.XPATH, "./div/div/input")
     dropDown_open = (By.XPATH, ".//input")
     item_search_scroll_element = (By.XPATH, "//div/div/div/ul")
     read_text = (By.XPATH, "./div/span")
@@ -15,4 +16,3 @@ class DropdownLocators:
     @staticmethod
     def select(pre):
         return By.XPATH, f"//li[@aria-label='{pre}']"
-
