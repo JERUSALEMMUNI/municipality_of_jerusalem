13b54cd34206dbcfa27da719b6fc8153308322fe
abdallahNatshehNessTech <abdallah.natsheh@ness-tech.co.il>
2023-03-19 14:38:31 +0200

    fix phone field step at write_phone_number_invalid_value
    Co-Authored-By: DarweeshQaimari <125342803+DarweeshQaimari@users.noreply.github.com>

--- features/steps/phone_field_step.py
+++ features/steps/phone_field_step.py
@@ -31,15 +31,27 @@ def write_phone_number(context, phone_number, widget_name):
 
 
 @when('fill "{phone_number}" as invalid value in "{widget_name}"')
-def write_phone_number(context, phone_number, widget_name):
+def write_phone_number_invalid_value(context, phone_number, widget_name):
     widget = context._config.current_page.widgets[widget_name]
     widget.set_full_phone(phone_number)
-    if not widget.is_invalid:
-        log.info(f"This value {phone_number} at field {widget_name} is considered "
-                 f" an invalid value but it appeared as valid")
-        rep.add_label_to_step("wrong_value", f"This value {phone_number} at field {widget_name} is considered "
-                                             f" an invalid value but it appeared as valid")
-        raise AssertionError("invalid value and considered as valid")
+    phone = phone_number.split("-")
+    prefix = phone[0]
+    suffix = phone[1]
+    try:
+        if not widget.is_invalid[1]:
+            log.info(f"Couldn't find this value {prefix} is not a valid option to select")
+            rep.add_label_to_step("wrong_value", f"This value {prefix} at field not a valid value to select")
+            raise AssertionError("This is not a valid value to select from list")
+        if not widget.is_invalid[0]:
+            log.info(f"This value {suffix} at field {widget_name} is considered "
+                     f"as invalid")
+            rep.add_label_to_step("wrong_value", f"This value {suffix} at field {widget_name} is considered "
+                                                 f" an invalid value but it appeared as valid")
+            raise AssertionError("valid value and considered as invalid")
+    finally:
+        # Todo: the close is not working correctly
+        # widget.close()
+        pass
 
 
 @when('from parent "{parent}" fill "{phone_number}" as valid value in "{widget_name}"')
