544ac5a8c12dfb9588e66b35c193ad60506e14a6
suhaibNess <p0035247@ness-tech.co.il>
2023-04-03 18:48:31 +0300

    TREE CUT

--- packages/screens/forms/tree_cut.py
+++ packages/screens/forms/tree_cut.py
@@ -46,6 +46,8 @@ class TreeCut(BasePage):
         self.widgets["אני מודע/ת ומסכים/ה לכך"] = create_widget('CaptchaBox', style=self.style,
                                                                 label="אני מודע/ת ומסכים/ה לכך")
         self.widgets['מספר העצים'] = create_widget('TextNumberField', style=self.style, label='מספר העצים')
+        self.widgets['שמור'] = create_widget('ButtonField', style=self.style, label='שמור')
+        self.widgets['שמור טיוטה'] = create_widget('ButtonField', style=self.style, label='שמור טיוטה')
 
     def fill_form_to_reach_step(self,context, dst_step, mailbox, driver, current_page):
         if dst_step == "פרטי הבקשה":
